0fc4921752499c5ea0a8cba8967378b0907cac18
karenlyuan <karenldyuan@gmail.com>
2024-01-12 23:21:48 -0800

    adding attempts for finished tasks

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from timer import Timer
+import time
 
 t1 = Timer()
+t1.set_timer
+t1.start_time
+t1.finish_timer
+
+if t1.finish_timer:
+    t1.more_attempts(1)
--- timer.py
+++ timer.py
@@ -1,8 +1,12 @@
+import time
+
 class Timer:
     def __init__():
         preference = input("Would you like to use the pomodoro method or set a custom timer?")
         timer_work = 0
         timer_break = 0
+        timer_complete = False
+        attempts = 0
 
     def set_timer(preference):
         while preference != "pomodoro" and preference != "custom":
@@ -15,3 +19,18 @@ class Timer:
         if preference == "custom":
             timer_work = input("How long would you like to work for?")
             timer_break = input("How long would you like to take a break for?")
+
+    def start_time(timer_work, timer_break):
+        time.time()
+        print("Time to starting working!")
+        time.sleep(timer_work)
+        time.time()
+        print("Time for your break! Good job, you earned it!")
+        time.sleep(timer_break)
+        time.time()
+
+    def finish_timer():
+        timer_complete = True
+
+    def more_attempts(num):
+        attempts += num
